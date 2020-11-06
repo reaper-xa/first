06fe976830396a8f659e2a058f87949b099561f6
reaper-x <ibrahimgadli21@gmail.com>
2020-11-06 23:37:24 +0530

    initial commit

--- guitodo.py
+++ guitodo.py
@@ -1,15 +1,11 @@
 import PySimpleGUI as sg
 
 layout = [
-    [sg.Text("Enter your name", font=("Arial",16)), 
-     sg.InputText("", font=("Arial",16), size=(40,1), key="name")],
+    [sg.Text("Enter your name", font=("Arial",16))],
     [sg.Button("ok")]
 ]
 
 window = sg.Window("First App", layout)
 event, values = window.Read()
-if event == "ok":
-    print(values["name"])
 window.close()
-"hello"
 
